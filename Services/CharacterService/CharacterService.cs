24edb1c75dd2b8473df691c0132ad1481fd22047
Tornado3232 <deawedaw3232@gmail.com>
2023-02-21 23:30:52 +0300

    CharacterService

---
+++ Services/CharacterService/CharacterService.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet_rpg.Services.CharacterService
+{
+    public class CharacterService : ICharacterService
+    {
+        private static List<Character> characters = new List<Character>
+        {
+            new Character(),
+            new Character {Id = 1, Name = "Sam"}
+        };
+
+        public List<Character> AddCharacter(Character newCharacter)
+        {
+            characters.Add(newCharacter);
+            return characters;
+        }
+
+        public List<Character> GetAllCharacters()
+        {
+            return characters;
+        }
+
+        public Character GetCharacterById(int id)
+        {
+            var character = characters.FirstOrDefault(c => c.Id == id);
+            if(character is not null)
+                return character;
+            throw new Exception("Character not found");
+        }
+    }
+}
---
+++ Services/CharacterService/ICharacterService.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet_rpg.Services.CharacterService
+{
+    public interface ICharacterService
+    {
+        List<Character> GetAllCharacters();
+
+        Character GetCharacterById(int id);
+
+        List<Character> AddCharacter(Character newCharacter);
+    }
+}
