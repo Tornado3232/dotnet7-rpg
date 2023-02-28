5a613a24783f7ba2214e6ae0c8fecb69cbbec78c
Tornado3232 <deawedaw3232@gmail.com>
2023-02-28 23:15:01 +0300

    Service Response

--- Services/CharacterService/ICharacterService.cs
+++ Services/CharacterService/ICharacterService.cs
@@ -7,10 +7,10 @@ namespace dotnet_rpg.Services.CharacterService
 {
     public interface ICharacterService
     {
-        Task<List<Character>> GetAllCharacters();
+        Task<ServiceResponse<List<Character>>> GetAllCharacters();
 
-        Task<Character> GetCharacterById(int id);
+        Task<ServiceResponse<Character>> GetCharacterById(int id);
 
-        Task<List<Character>> AddCharacter(Character newCharacter);
+        Task<ServiceResponse<List<Character>>> AddCharacter(Character newCharacter);
     }
 }
