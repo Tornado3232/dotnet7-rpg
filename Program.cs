76f65b05b086d02b8f1bdd99c79ceccf4e351185
Tornado3232 <deawedaw3232@gmail.com>
2023-03-09 23:57:37 +0300

    First Migration

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ global using dotnet_rpg.Services.CharacterService;
 global using dotnet_rpg.Dtos.Character;
 global using AutoMapper;
 global using Microsoft.EntityFrameworkCore;
-global using dotnet_rpg.Data
+global using dotnet_rpg.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
