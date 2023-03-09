93ceee85ad3adebb8c56427d0822deeefee7a0f2
Tornado3232 <deawedaw3232@gmail.com>
2023-03-09 23:39:02 +0300

    DataContext Implementation

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ global using dotnet_rpg.Models;
 global using dotnet_rpg.Services.CharacterService;
 global using dotnet_rpg.Dtos.Character;
 global using AutoMapper;
+global using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
