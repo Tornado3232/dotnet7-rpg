d2f0ea5124b79d87ab07dfc6bc5ef8ea9c5ce014
Tornado3232 <deawedaw3232@gmail.com>
2023-03-14 00:03:11 +0300

    User Already Exists

--- Data/AuthRepository.cs
+++ Data/AuthRepository.cs
@@ -21,6 +21,15 @@ namespace dotnet_rpg.Data
 
         public async Task<ServiceResponse<int>> Register(User user, string password)
         {
+            var response = new ServiceResponse<int>();
+            
+            if(await UserExists(user.UserName))
+            {
+                response.Success = false;
+                response.Message = "User already exists.";
+                return response;
+            }
+            
             CreatePasswordHash(password, out byte[] passwordHash, out byte[] passwordSalt);
 
             user.PasswordHash = passwordHash;
@@ -28,14 +37,17 @@ namespace dotnet_rpg.Data
 
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
-            var response = new ServiceResponse<int>();
             response.Data = user.Id;
             return response;
         }
 
-        public Task<bool> UserExists(string username)
+        public async Task<bool> UserExists(string username)
         {
-            throw new NotImplementedException();
+            if(await _context.Users.AnyAsync(u => u.UserName.ToLower() == username.ToLower()))
+            {
+                return true;
+            }
+            return false;
         }
 
         private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
